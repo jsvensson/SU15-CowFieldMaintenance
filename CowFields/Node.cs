338bc3ececc5b473ad9e21d6557219160653c66b
Johan Svensson <johan@atomicplayboy.net>
2017-01-19 11:20:53 +0100

    Add project files.

---
+++ CowFields/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CowFields
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public bool Visited { get; set; }
+        public Dictionary<int, int> Paths { get; set; }
+    }
+}
